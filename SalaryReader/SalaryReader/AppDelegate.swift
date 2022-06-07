a42efb2f977a6f47c8b990ab3ee2536939ef455a
TrevorWu <wowpapa3232@gmail.com>
2022-06-07 21:39:53 +0800

    feat: get file url

--- SalaryReader/SalaryReader/AppDelegate.swift
+++ SalaryReader/SalaryReader/AppDelegate.swift
@@ -9,7 +9,31 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    @IBAction func openFile(_ sender: NSMenuItem) {
+        let dialog = NSOpenPanel();
+        dialog.title = "Chose your .enc file"
+        dialog.message = "Where is your fuckin .enc file?"
+        dialog.allowedFileTypes = ["enc"]
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = true;
+        dialog.allowsMultipleSelection = false;
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url
+            print(result ?? "oh shit")
+//            if (result != nil) {
+//                let path = result!.path!
+//                filename_field.stringValue = path
+//            }
+        } else {
+            // User did nothing
+            return
+        }
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
--- SalaryReader/SalaryReader/ViewController.swift
+++ SalaryReader/SalaryReader/ViewController.swift
@@ -4,23 +4,22 @@
 //
 //  Created by Trevor on 2022/6/7.
 //
-
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var dragView: NSView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
 }
-
