9154646f82de8f5906d34cec2b78fe6d7596e071
TrevorWu <wowpapa3232@gmail.com>
2022-06-07 19:21:38 +0800

    Init

---
+++ SalaryReader/SalaryReader/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SalaryReader
+//
+//  Created by Trevor on 2022/6/7.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
